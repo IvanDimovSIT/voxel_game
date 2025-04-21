4e7ab0c4bdc023dee9f4da572954bc816d6ea187
IvanDimovSIT <ivan390@abv.bg>
2025-04-21 13:32:46 +0300

    Added world selection screen

---
+++ src/interface.rs
@@ -0,0 +1,123 @@
+use macroquad::{camera::set_default_camera, color::{Color, DARKGRAY, RED}, math::{vec2, RectOffset, Vec2}, miniquad::window::screen_size, ui::{hash, root_ui, widgets::{self, Group}, Skin, Style}, window::{clear_background, next_frame}};
+
+use crate::voxel_engine::VoxelEngine;
+
+const WORLD_NAME_ID: u64 = 1;
+const MENU_SIZE: Vec2 = vec2(500.0, 300.0);
+const NEUTRAL_COLOR: Color = Color::from_rgba(220, 220, 220, 255);
+const SELECTED_COLOR: Color = Color::from_rgba(230, 230, 255, 255);
+const MESSAGE_POSITION: Vec2 = vec2(0.0, 80.0); 
+
+pub struct InterfaceContext {
+    skin: Skin,
+    world_name: String,
+    error: String,
+    should_enter: bool
+}
+impl InterfaceContext {
+    pub fn new() -> Self {
+        Self {
+            skin: Self::initialise_skin(),
+            world_name: "".to_owned(),
+            error: "".to_owned(),
+            should_enter: false, 
+        }
+    }
+
+    pub async fn enter_game(&self) -> Option<Box<VoxelEngine>> {
+        if self.should_enter {
+            let voxel_engine = Box::new(VoxelEngine::new(&self.world_name).await);
+            Some(voxel_engine)
+        } else {
+            None
+        }
+    }
+
+    pub async fn draw(&mut self) {
+        set_default_camera();
+        clear_background(DARKGRAY);
+        let (width, height) = screen_size();
+        
+        root_ui().push_skin(&self.skin);
+        widgets::Window::new(
+            hash!(),
+            vec2((width - MENU_SIZE.x) / 2.0, height * 0.1),
+            MENU_SIZE
+        )
+            .label("Voxel Game")
+            .movable(false)
+            .titlebar(true)
+            .ui(&mut *root_ui(), |ui| {
+                ui.input_text(WORLD_NAME_ID, "World name", &mut self.world_name);
+                if ui.button(vec2(MENU_SIZE.x * 0.5, 150.0), "Play".to_owned()) {
+                    self.validate();
+                    if self.error.is_empty() {
+                        self.should_enter = true;
+                        ui.label(MESSAGE_POSITION, "Loading...");
+                    }
+                }
+                
+                ui.label(MESSAGE_POSITION, &self.error);
+            });
+
+        self.remove_invalid_characters();
+        if !self.error.is_empty() {
+            self.validate();
+        }
+        root_ui().pop_skin();
+        next_frame().await;
+    }
+
+    fn initialise_skin() -> Skin {
+        let window_style = root_ui()
+            .style_builder()
+            .font_size(20)
+            .margin(RectOffset::new(10.0, 10.0, 10.0, 10.0))
+            .background_margin(RectOffset::new(10.0, 10.0, 10.0, 10.0))
+            .build();
+
+        let text_style = root_ui()
+            .style_builder()
+            .font_size(20)
+            .margin(RectOffset::new(0.0, 0.0, 0.0, 10.0))
+            .background_margin(RectOffset::new(0.0, 0.0, 10.0, 10.0))
+            .color(NEUTRAL_COLOR)
+            .color_selected(SELECTED_COLOR)
+            .color_hovered(SELECTED_COLOR)
+            .color_selected_hovered(SELECTED_COLOR)
+            .color_clicked(SELECTED_COLOR)
+            .build();
+
+        let button_style = root_ui()
+            .style_builder()
+            .font_size(35)
+            .margin(RectOffset::new(10.0, 10.0, 10.0, 10.0))
+            .background_margin(RectOffset::new(10.0, 10.0, 10.0, 10.0))
+            .color(NEUTRAL_COLOR)
+            .color_hovered(SELECTED_COLOR)
+            .build();
+        
+        Skin {
+            window_style,
+            button_style,
+            label_style: text_style.clone(),
+            editbox_style: text_style,
+            ..root_ui().default_skin()
+        }
+    }
+
+    fn validate(&mut self) {
+        if self.world_name.len() < 3 {
+            self.error = "World name should be at least 3 characters".to_owned();
+        } else {
+            self.error = "".to_owned();
+        }
+    }
+
+    fn remove_invalid_characters(&mut self) {
+        self.world_name = self.world_name
+            .chars()
+            .filter(|c| c.is_alphanumeric() || *c == '_')
+            .collect()            
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use interface::InterfaceContext;
 use macroquad::{conf::Conf, texture::FilterMode, time::get_frame_time};
 use voxel_engine::VoxelEngine;
 
@@ -6,6 +7,7 @@ mod model;
 mod service;
 mod utils;
 mod voxel_engine;
+mod interface;
 
 fn config() -> Conf {
     Conf {
@@ -16,15 +18,36 @@ fn config() -> Conf {
     }
 }
 
+enum GameState {
+    Running {voxel_engine: Box<VoxelEngine>},
+    Menu {context: Box<InterfaceContext>}
+}
+impl Default for GameState {
+    fn default() -> Self {
+        Self::Menu { context: Box::new(InterfaceContext::new()) }
+    }
+}
+
 #[macroquad::main("Voxel World", config)]
 async fn main() {
-    let mut voxel_engine = VoxelEngine::new("test_world").await;
+    let mut state = GameState::default();
 
     loop {
-        let delta = get_frame_time().min(0.2);
-        let raycast_result = voxel_engine.process_input(delta);
-        voxel_engine.process_physics(delta);
-        voxel_engine.update_loaded_areas();
-        voxel_engine.draw_scene(raycast_result).await;
+        match &mut state {
+            GameState::Running { voxel_engine } => {
+                let delta = get_frame_time().min(0.2);
+                let raycast_result = voxel_engine.process_input(delta);
+                voxel_engine.process_physics(delta);
+                voxel_engine.update_loaded_areas();
+                voxel_engine.draw_scene(raycast_result).await;
+            },
+            GameState::Menu { context } => {
+                if let Some(voxel_engine) = context.enter_game().await {
+                    state = GameState::Running { voxel_engine }
+                } else {
+                    context.draw().await;
+                }
+            }
+        }
     }
 }
