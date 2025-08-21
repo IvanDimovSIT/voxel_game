bf0dc39df88b7f26e2eb715903e278b2fc93fb29
IvanDimovSIT <ivan390@abv.bg>
2025-08-22 00:08:38 +0300

    Improved sky shader

--- resources/shaders/sky_fragment.glsl
+++ resources/shaders/sky_fragment.glsl
@@ -3,9 +3,14 @@
 precision mediump float;
 
 varying vec2 uv;
+varying vec3 facePosition;
 
 uniform sampler2D Texture;
 
+const float fadeFactor = 0.05;
+
 void main() {
-    gl_FragColor = texture2D(Texture, uv);
+    vec4 sample = texture2D(Texture, uv);
+    float alpha = min(clamp(-facePosition.z*fadeFactor, 0.0, 1.0), sample.a);
+    gl_FragColor = vec4(sample.rgb, alpha);
 }
--- resources/shaders/sky_vertex.glsl
+++ resources/shaders/sky_vertex.glsl
@@ -4,11 +4,13 @@ attribute vec3 position;
 attribute vec2 texcoord;
 
 varying lowp vec2 uv;
+varying vec3 facePosition;
 
 uniform mat4 Model;
 uniform mat4 Projection;
 
 void main() {
+    facePosition = position;
     gl_Position = Projection * Model * vec4(position, 1.0);
     uv = texcoord;
 }
